ffeb28d0c4144544a6fe29e6fe68fa2a54d724a2
rinaldi163 <rinaldi.aldi47@yahoo.com>
2018-02-25 23:04:35 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/toshiba/rinalditambunan_1106130092_modul3/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.toshiba.rinalditambunan_1106130092_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //handle untuk memulai MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
